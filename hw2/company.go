e480707435ad0e93eb716f129f936100e1f128f5
vebcreatex7 <kirill.vlg3101@gmail.com>
2021-09-30 21:29:22 +0300

    added runtime checking

--- hw2/company.go
+++ hw2/company.go
@@ -5,13 +5,25 @@ import (
 	"time"
 )
 
-type Working struct {
-	WorkingType      string
-	WorkingValue     int
-	WorkingID        interface{}
-	WorkingCreatedAt string
+// This struct will be used after testing on validity
+type WorkingCompany struct {
+	WCompany   string
+	WType      string
+	WValue     int
+	WId        interface{}
+	WCreatedAt string
+	IsValid    bool
 }
 
+// This structs are using for parsing json
+type ParsingCompany struct {
+	Company   interface{} `json:"company,omitempty"`
+	Type      string      `json:"type,omitempty"`
+	Value     interface{} `json:"value,omitempty"`
+	ID        interface{} `json:"id,omitempty"`
+	CreatedAt string      `json:"created_at,omitempty"`
+	Operation `json:"operation,omitempty"`
+}
 type Operation struct {
 	Type      string      `json:"type,omitempty"`
 	Value     interface{} `json:"value,omitempty"`
@@ -19,22 +31,7 @@ type Operation struct {
 	CreatedAt string      `json:"created_at,omitempty"`
 }
 
-type Company struct {
-	Company   interface{} `json:"company,omitempty"`
-	Type      string      `json:"type,omitempty"`
-	Value     interface{} `json:"value,omitempty"`
-	ID        interface{} `json:"id,omitempty"`
-	CreatedAt string      `json:"created_at,omitempty"`
-	Operation `json:"operation,omitempty"`
-
-	IsSkipped bool
-	IsValid   bool
-
-	// This struct will be used after testing on validity
-	// To make it esier to search in struct
-	Working `json:"working"`
-}
-
+// This struct contains info of each company
 type Result struct {
 	Company  string        `json:"company"`
 	Count    int           `json:"valid_operations_count"`
@@ -44,52 +41,49 @@ type Result struct {
 
 //
 // The function tests for validity and marks it as valid
-func SetValid(c *Company) {
+func CheckValid(c ParsingCompany) (WorkingCompany, bool) {
+	var ans WorkingCompany
+	ans.IsValid = true
 	// Checking the name of a company
 	if s, ok := c.Company.(string); !ok || s == "" {
-		c.IsSkipped = true
-		return
+		return WorkingCompany{}, false
 	}
+	ans.WCompany = c.Company.(string)
 
 	// Checking Created_at
 	if _, err := time.Parse(time.RFC3339, c.CreatedAt); err != nil {
 		if _, err := time.Parse(time.RFC3339, c.Operation.CreatedAt); err != nil {
-			c.IsSkipped = true
-			return
+			return WorkingCompany{}, false
 		}
-		c.WorkingCreatedAt = c.CreatedAt
+		ans.WCreatedAt = c.CreatedAt
 	} else {
-		c.WorkingCreatedAt = c.CreatedAt
+		ans.WCreatedAt = c.CreatedAt
 	}
 
 	// Checking  id
 	switch v := c.ID.(type) {
 	case float64:
 		if v != float64(int(v)) {
-			c.IsSkipped = true
-			return
+			return WorkingCompany{}, false
 		}
-		c.WorkingID = int(v)
+		ans.WId = int(v)
 	case string:
 		// Ok
-		c.WorkingID = v
+		ans.WId = v
 
 	// Also for Operation
 	default:
 		switch v := c.Operation.ID.(type) {
 		case float64:
 			if v != float64(int(v)) {
-				c.IsSkipped = true
-				return
+				return WorkingCompany{}, false
 			}
-			c.WorkingID = int(v)
+			ans.WId = int(v)
 		case string:
 			// Ok
-			c.WorkingID = v
+			ans.WId = v
 		default:
-			c.IsSkipped = true
-
-			return
+			return WorkingCompany{}, false
 		}
 	}
 
@@ -97,78 +91,67 @@ func SetValid(c *Company) {
 	if c.Type != "income" && c.Type != "outcome" && c.Type != "+" && c.Type != "-" {
 		// Also for Operation
 		if c.Operation.Type != "income" && c.Operation.Type != "outcome" && c.Operation.Type != "+" && c.Operation.Type != "-" {
-			c.IsValid = false
-			return
+			ans.IsValid = false
 		}
-		c.WorkingType = c.Operation.Type
+		ans.WType = c.Operation.Type
 	} else {
-		c.WorkingType = c.Type
+		ans.WType = c.Type
 	}
 
-	//
 	// Checking the value
 	switch v := c.Value.(type) {
 	case float64:
 		if v != float64(int(v)) {
-			c.IsValid = false
-			return
+			ans.IsValid = false
 		}
-		c.WorkingValue = int(v)
+		ans.WValue = int(v)
 	case string:
-
 		f, err2 := strconv.ParseFloat(v, 64)
 		if err2 != nil {
-			c.IsValid = false
-			return
+			ans.IsValid = false
 		}
 		if f != float64(int(f)) {
-			c.IsValid = false
-			return
+			ans.IsValid = false
 		}
-		c.WorkingValue = int(f)
+		ans.WValue = int(f)
 
 	default:
 		// Also for Operation
 		switch v := c.Operation.Value.(type) {
 		case float64:
 			if v != float64(int(v)) {
-				c.IsValid = false
-				return
+				ans.IsValid = false
 			}
-			c.WorkingValue = int(v)
-			c.Value = int(v)
+			ans.WValue = int(v)
 		case string:
 			f, err2 := strconv.ParseFloat(v, 64)
 			if err2 != nil {
-				c.IsValid = false
-				return
+				ans.IsValid = false
 			}
 			if f != float64(int(f)) {
-				c.IsValid = false
-				return
+				ans.IsValid = false
 			}
-			c.WorkingValue = int(f)
+			ans.WValue = int(f)
 
 		default:
-			c.IsValid = false
-			return
+			ans.IsValid = false
 		}
 	}
 
-	c.IsValid = true
+	return ans, true
 }
 
-func Fill(r *Result, c []Company) {
+func Fill(r *Result, c []WorkingCompany) {
 	for _, val := range c {
 		if val.IsValid {
 			r.Count++
-			if val.WorkingType == "income" || val.WorkingType == "+" {
-				r.Balance += val.WorkingValue
+			if val.WType == "income" || val.WType == "+" {
+				r.Balance += val.WValue
 			} else {
-				r.Balance -= val.WorkingValue
+				r.Balance -= val.WValue
 			}
 		} else {
-			r.Invalids = append(r.Invalids, val.WorkingID)
+			r.Invalids = append(r.Invalids, val.WId)
 		}
 	}
 }
--- hw2/main.go
+++ hw2/main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"sort"
 	"time"
+
+	"strings"
 )
 
 func asEnv() (string, bool) {
@@ -62,26 +64,44 @@ func main() {
 	file.Close()
 
 	// Reading from a JSON to a slice of company
-	var sCompanies []Company
-	if err := json.Unmarshal(data, &sCompanies); err != nil {
-		fmt.Println("Error in unmarshaling")
+	mCompannies := make(map[string][]WorkingCompany)
+	dec := json.NewDecoder(strings.NewReader(string(data)))
+
+	// Reading opening bracket
+	_, err = dec.Token()
+	if err != nil {
+		fmt.Println("err")
 		return
 	}
 
-	// Processing data
-	mCompannies := make(map[string][]Company)
-	for _, val := range sCompanies {
-		SetValid(&val)
-		if !val.IsSkipped {
-			mCompannies[val.Company.(string)] = append(mCompannies[val.Company.(string)], val)
+	// while array contains values
+	for dec.More() {
+		var c ParsingCompany
+		err = dec.Decode(&c)
+		if err != nil {
+			fmt.Println("err")
+			return
 		}
+		company, flag := CheckValid(c)
+		if flag {
+			mCompannies[company.WCompany] = append(mCompannies[company.WCompany], company)
+		} else {
+			continue
+		}
+	}
+
+	// Reading closing bracket
+	_, err = dec.Token()
+	if err != nil {
+		fmt.Println("err")
+		return
 	}
 
 	// Sorting by date
 	for s := range mCompannies {
 		sort.Slice(mCompannies[s], func(i, j int) bool {
-			t1, _ := time.Parse(time.RFC3339, mCompannies[s][i].WorkingCreatedAt)
-			t2, _ := time.Parse(time.RFC3339, mCompannies[s][j].WorkingCreatedAt)
+			t1, _ := time.Parse(time.RFC3339, mCompannies[s][i].WCreatedAt)
+			t2, _ := time.Parse(time.RFC3339, mCompannies[s][j].WCreatedAt)
 			return t1.Before(t2)
 		})
 	}
@@ -99,18 +119,19 @@ func main() {
 		return sResults[i].Company < sResults[j].Company
 	})
 
+	// Unmarshaling
 	newData, err := json.MarshalIndent(sResults, "", "\t")
 	if err != nil {
 		fmt.Println("Error in marshaling")
 		return
 	}
 
+	// Writing to a file
 	out, err := os.Create("out.json")
 	if err != nil {
 		fmt.Println("Can't open out.json")
 		return
 	}
-
 	if _, err = out.Write(newData); err != nil {
 		fmt.Println("Can't write to out.json")
 		return
